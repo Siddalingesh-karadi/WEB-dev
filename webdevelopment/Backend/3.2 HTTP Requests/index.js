492ad7f70bf78e2a9f59cedd2c0b3a3e70395a48
Siddalingesh24 <siddalingeshkaradi@gmail.com>
2026-05-24 23:49:33 +0530

    middleware part1 added

--- webdevelopment/Backend/3.2 HTTP Requests/index.js
+++ webdevelopment/Backend/3.2 HTTP Requests/index.js
@@ -10,7 +10,10 @@ app.get("/home", (req, res) => {
 res.send("<h1>home page</h1><p>this is the home page</p>");
 });
 
-
+app.get("/karnataka",(req,res)=>{
+    res.send("this is karnataka state <a href='https://www.karnataka.gov.in/'>visit website</a>");
+    
+});
 app.listen(port, () => {
 console.log(`Server started on port ${port}`);
 });
