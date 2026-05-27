3760faa8c8df66b68897f18f3734f177993a6d69
Siddalingesh24 <siddalingeshkaradi@gmail.com>
2026-05-27 18:44:12 +0530

    upto middle-ware

--- webdevelopment/Backend/3.4 Middleware/index3.js
+++ webdevelopment/Backend/3.4 Middleware/index3.js
@@ -1,8 +1,15 @@
+import bodyParser from "body-parser";
 import express from "express";
-
 const app = express();
 const port = 3000;
 
+
+
+function logger(req,res,next){
+  console.log("request method:",req.method);
+  console.log("request url:",req.url);
+  next();
+}
 app.use(logger);
 
 app.get("/", (req, res) => {
